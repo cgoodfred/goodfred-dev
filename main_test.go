151eff4d4ed5db206b2f57f467faf7a834e082b5
Corey Goodfred <53098963+cgoodfred@users.noreply.github.com>
2022-06-15 21:38:06 -0400

    Add minimal dockerfile (#5)
    * remove localhost and add newline character
    
    * add basic dockerfile

--- main_test.go
+++ main_test.go
@@ -20,7 +20,7 @@ func TestHome(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := `Hello World`
+	expected := "Hello World\n"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
