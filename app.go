e954ed59afc354b7391eb3ab482ac662b8643036
Corey Goodfred <53098963+cgoodfred@users.noreply.github.com>
2022-07-15 06:18:47 -0400

    Initial postgres setup/api routes/environment configuration (#11)
    * rework into app and model with basic api routes and env configuration and postgres setup
    
    * add models tests with go-sqlmock
    
    * some renaming

---
+++ app.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) Initialize() {
+	connectionString := getDBConfig()
+
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	a.Router = mux.NewRouter()
+
+	a.initializeRoutes()
+}
+
+func (a *App) Run() {
+	log.Fatal(http.ListenAndServe(":8080", a.Router))
+}
+
+func getDBConfig() string {
+	viper.SetDefault("DB_USER", "postgres")
+	viper.SetDefault("DB_PASSWORD", "postgres")
+	viper.SetDefault("DB_HOST", "localhost")
+	viper.SetDefault("DB_PORT", "15432")
+	viper.SetDefault("DB_NAME", "sensor")
+	host := viper.Get("DB_HOST")
+	port := viper.GetInt("DB_PORT")
+	user := viper.Get("DB_USER")
+	password := viper.Get("DB_PASSWORD")
+	dbname := viper.Get("DB_NAME")
+
+	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+}
+
+func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/sensor", a.createSensorReading).Methods("POST")
+	a.Router.HandleFunc("/sensor/{id:[0-9]+}", a.getSensorReading).Methods("GET")
+	a.Router.HandleFunc("/sensor/{id:[0-9]+}", a.deleteSensorReading).Methods("DELETE")
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func (a *App) getSensorReading(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid sensor ID")
+		return
+	}
+
+	s := sensorReading{SID: id}
+	if err := s.getReading(a.DB); err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			respondWithError(w, http.StatusNotFound, fmt.Sprintf("No records found for sensor_id: %d", id))
+		default:
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, s)
+}
+
+func (a *App) createSensorReading(w http.ResponseWriter, r *http.Request) {
+
+	var s sensorReading
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&s); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	// if a payload has no time, default to current UTC time
+	if s.Time.IsZero() {
+		s.Time = time.Now().UTC()
+	}
+	defer r.Body.Close()
+	if err := s.createReading(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, s)
+}
+
+func (a *App) deleteSensorReading(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Sensor ID")
+		return
+	}
+
+	s := sensorReading{SID: id}
+	if err := s.deleteReading(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
---
+++ model.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"time"
+)
+
+type sensorReading struct {
+	SID    int       `json:"sensor_id"`
+	Time   time.Time `json:"reading_time"`
+	Weight float64   `json:"weight"`
+}
+
+func (s *sensorReading) getReading(db *sql.DB) error {
+	return db.QueryRow("SELECT record_time, weight FROM sensor_reading WHERE sensor_id=$1 ORDER BY record_time DESC",
+		s.SID).Scan(&s.Time, &s.Weight)
+}
+
+func (s *sensorReading) deleteReading(db *sql.DB) error {
+	_, err := db.Exec("DELETE FROM sensor_reading WHERE sensor_id=$1", s.SID)
+
+	return err
+}
+
+func (s *sensorReading) createReading(db *sql.DB) error {
+	_, err := db.Exec("INSERT INTO sensor_reading(sensor_id, record_time, weight) VALUES($1, $2, $3) RETURNING id", s.SID, s.Time, s.Weight)
+
+	return err
+}
